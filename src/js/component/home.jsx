1423b2094fccf451e39385efd9f440baa558114f
odettelittle <Odettelittle1@gmail.com>
2022-03-25 23:25:40 +0000

    cuarto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [selectedColor, setSelectedColor] = useState("red");
+
 	return (
 		<div className="traffic-light">
 			<div className="light red"></div>
